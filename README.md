845cd32a381939e15719c7c787eb4e7093e4f543
Linsey <linseyjayne@gmail.com>
2026-03-24 11:00:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+what is this
+- hamstring exercise program
+- im tired of clicking stop and restart on my phone timer when i do these exercises
+- my hamstring hurts a lot from sitting at a computer
+- i have to make doing these exercises easier for myself and this is how i thought i could do it
+
+why is it this way
+
+- i didnt believe in myself to make this with just javascript
+- look i did it
+- i wanted to make something simple without giving it away to ai 
+- it didnt take me forever to make a bunch of decisions and i have something i can use now
+
+todo list 
+- css of course
+- add sound on change
+- cute animation
+- math to show how long it's gonna take
+
+questions to answer
+- is there a better way to manage state variables in javascript
+- should i separate this stuff? it's not that big tbh
+- what if there was a slider so you could skip ahead in your routine
+- what if we labeled like dynamic things vs things that actually should be held for 30 seconds
+- what if we had a short version
---
+++ script.js
@@ -0,0 +1,86 @@
+// query selectors
+const startButton = document.querySelector('#startBtn');
+const pauseButton = document.querySelector('#pauseBtn');
+const displayText = document.querySelector('#displayText');
+const timeOutput = document.querySelector('#timeOutput');
+const statusText = document.querySelector('#statusText');
+
+const exercises = [
+  { name: "Hamstring stretch with foot on step", sets: 3, time: 30, rest: 7 },
+  { name: "Kneeling Quad Stretch", sets: 3, time: 30, rest: 7 },
+  { name: "Single Leg Heel Raise", sets: 3, time: 30, rest: 7 },
+  { name: "Hip Flexor Stretch", sets: 3, time: 30, rest: 7 },
+  { name: "Standing Calf Stretch", sets: 3, time: 30, rest: 7 },
+  { name: "Quad Pull", sets: 3, time: 30, rest: 7 },
+  { name: "Hamstring Dip", sets: 3, time: 30, rest: 7 },
+  { name: "Frankenstein Kicks", sets: 3, time: 30, rest: 7 },
+  { name: "Side Lying Hip Abduction", sets: 3, time: 30, rest: 7 },
+];
+
+let currentExerciseIndex = 0;
+let currentExercise = exercises[currentExerciseIndex];
+let running = false; 
+let timer = 0;
+let currentSet = 0;
+let isResting = false;
+let startingTime = null;
+
+startButton.addEventListener('click', () => {
+  running = !running;
+  displayText.textContent = currentExercise.name;
+  startingTime = Date.now();
+  startButton.textContent = "stop";
+});
+
+pauseButton.addEventListener('click', () => {
+  running = !running;
+  displayText.textContent = `${currentExercise.name} paused!`;
+});
+
+let lastTime = Date.now();
+
+const tick = () => {
+    const now = Date.now();
+    const delta = now - lastTime;
+    lastTime = now;
+    const timerTime = isResting ? currentExercise.rest : currentExercise.time;
+
+    if (running) { 
+      timer += delta;
+      const currentSeconds = (timer / 1000).toFixed(0);
+      timeOutput.textContent = currentSeconds + 's';
+      if(currentSeconds == timerTime + 1) {
+        if (currentSet < currentExercise.sets) {
+          if (!isResting) {
+            isResting = true;
+            statusText.textContent = 'resting';
+            currentSet++;
+          } else if (isResting) {
+            isResting = false;
+            statusText.textContent = 'working';
+          } 
+        } else {
+          // reset values
+          currentSet = 0;
+          isResting = false;
+          statusText.textContent = "";
+          timeOutput.textContent = "";
+
+          if (currentExerciseIndex + 1 < exercises.length) {
+            currentExerciseIndex++;
+            currentExercise = exercises[currentExerciseIndex];
+            displayText.textContent = currentExercise.name;
+          } else {
+            displayText.textContent = "you are done";
+            running = false;
+            startButton.textContent = "start";
+          }
+        } 
+        timer = 0;
+      }
+    }
+    
+    requestAnimationFrame(tick);
+  }
+
+tick();
